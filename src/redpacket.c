80d930c040242589edccf350f64fabbc0cde44ae
Tristan B. Kildaire <deavmi@disroot.org>
2020-09-13 18:05:40 +0200

    Implemented encode() function to encode a given redPacket into a byte stream for sending over Ethernet

--- src/redpacket.c
+++ src/redpacket.c
@@ -5,6 +5,8 @@
 */
 
 #include "redpacket.h"
+#include<arpa/inet.h>
+#include<stdlib.h>
 
 /**
 * Decodes the given bytes into a redPakcet
@@ -20,4 +22,42 @@ struct redPacket* decode(char* buffer)
 
 
 	return packet;
+}
+
+/**
+* Encodes the given redPacket into
+* a byte stream that can be sent
+*
+* @param redPacket the packet to
+* encode
+*/
+char* encode(struct redPacket* packet)
+{
+	/* Allocate the buffer */
+	char* buffer = malloc(1+8+8+1+4+4+packet->length);
+
+	/* Encode the version */
+	*buffer = packet->version;
+
+	/* Encode the source address */
+	*((long*)(buffer+1)) = packet->source;
+
+	/* Encode the destination address */
+	*((long*)(buffer+1+8)) = packet->destination;
+
+	/* Encode the time-to-live value */
+	*(buffer+1+8+8) = packet->ttl;
+
+	/* Encode the protocol type */
+	*((int*)(buffer+1+8+8+1)) = packet->type;
+
+	/* Encode the length as Big Endian */
+	*((int*)(buffer+1+8+8+1+4)) = htonl(packet->length);
+
+	/* Encode the payload (TODO: Perhaps use memcpy) */
+	for(unsigned int i = 0; i < packet->length; i++)
+	{
+		*(buffer+1+8+8+1+4+4+i) = *(packet->payload+i);
+	}
+	
 }
